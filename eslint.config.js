b61e400639948a5636737531aa527ccc01cac311
Wang Hoeun <38005874+hoeun0723@users.noreply.github.com>
2024-10-31 16:36:43 +0900

    [chore] 초기세팅 (#2)
    * chore: router 설치 및 router 설정
    
    * chore: styled-component 설치 및 start 페이지 생성
    
    * chore: style 초기세팅
    
    * chore: recoil 설치
    
    * chore: layout 구성
    
    * chore: 프리티어와 eslint 설정
    
    ---------
    
    Co-authored-by: Wang HoEun <hoeunwang@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,95 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-plugin-prettier';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import importPlugin from 'eslint-plugin-import';
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist', '.eslintrc.cjs'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
     },
-    settings: { react: { version: '18.3' } },
+    settings: {
+      react: { version: '18.3' },
+    },
     plugins: {
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier,
+      'jsx-a11y': jsxA11y,
+      import: importPlugin,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'prettier/prettier': [
+        'warn',
+        {
+          singleQuote: true,
+          parser: 'flow',
+          endOfLine: 'auto',
+        },
+      ],
+      'no-console': 'off',
+      'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+      'linebreak-style': 'off',
+      'import/no-unresolved': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'default-param-last': 'off',
+      'react/forbid-prop-types': 'off',
+      camelcase: 'off',
+      'no-shadow': 'off',
+      'no-param-reassign': ['warn', { props: false }],
+      'no-debugger': 'off',
+      'import/prefer-default-export': 'off',
+      'react/require-default-props': 'off',
+      'import/no-anonymous-default-export': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      '@eslint-plugin-js/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      '@eslint-plugin-js/naming-convention': [
+        'warn',
+        {
+          selector: 'default',
+          format: ['camelCase', 'PascalCase'],
+          leadingUnderscore: 'allow',
+          trailingUnderscore: 'allow',
+        },
+        {
+          selector: 'import',
+          format: ['camelCase', 'PascalCase'],
+        },
+        {
+          selector: 'variable',
+          format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+          leadingUnderscore: 'allow',
+          trailingUnderscore: 'allow',
+        },
+      ],
     },
   },
-]
+];
---
+++ src/pages/Start/Start.jsx
@@ -0,0 +1,7 @@
+function Start () {
+    return (
+        <div><h1>시작 페이지 입니다.</h1> </div>
+    )
+}
+
+export default Start;
